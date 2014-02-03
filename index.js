6a080e772ae94c65bc6451a05a6c2a0ebe41a4dc
Johann Philipp Strathausen <strathausen@gmail.com>
2014-02-03 14:53:07 +0100

    initial ronny scetch-up

--- index.js
+++ index.js
@@ -1,58 +1,25 @@
-var _ = require('underscore')
-  , time = require('english-time')
-  , url = require('url')
-  , Ronny
-  , slice = Array.prototype.slice;
-  , stores = {
-      redis: function RonnyRedis(cb) {
-        var redis = require('redis'),
-            client = redis.createClient(db.port, db.hostname, {
-              no_ready_check: true
-            });
-        
-        if(db.auth) {
-          client.auth(redisURL.auth.split(":")[1]);
-        }
-
-        return {
-          set: function(key, value, cb) {
-            client.set(key, JSON.stringify(value), cb);
-          },
-          get: function(key, cb) {
-            client.get(key, function(err, res) {
-              if (err) {
-                cb(err);
-                return;
-              }
-              var result;
-              try {
-                result = JSON.parse(res);
-              } catch(e) {
-                return cb(e);
-              }
-              cb(null, result)
-            });
-          }
-        }
-      }
-    }
+var _      = require('underscore'),
+    time   = require('english-time'),
+    url    = require('url'),
+    stores = require('./stores/Stores'),
+    slice  = Array.prototype.slice,
+    Ronny;
 
 module.exports = Ronny = function RonnyCache(opts) {
-  var u, Store, store;
   _.extends(opts, { maxAge: '10s' });
-  u = url.parse(opts.db);
-  this.maxAge = time(opts.maxAge);
+  var u, Store, store,
+      db = url.parse(opts.db),
+      maxAge = time(opts.maxAge);
+
   Store = stores[u.protocoll];
   if(Store) {
-    this.store = Store(u);
+    this.store = new Store(db);
+  } else {
+    throw new Error 'RonnyCache store not supported: ' + u.protocoll;
   }
-}
+};
 
 Ronny.prototype.wrap = function RonnyWrap(fun) {
-  // Transparently do not cache when no DB is provided
-  if(!this.store) {
-    return fun;
-  }
   var self = this;
   return function RonnyWrapped() {
     var args = slice.call(arguments, 0, arguments.length - 1),
@@ -60,32 +27,26 @@ Ronny.prototype.wrap = function RonnyWrap(fun) {
         key  = JSON.stringify(args);
 
     self.store.get(key, function(err, result) {
-      if (err) {
-        cb(err);
-        return;
+      if(err) {
+        return cb(err);
       }
-      // There is valid data, pass it on to the callback
-      if(result && result.data && result.expires > Date.now()) {
-        cb(null, result.data);
-      // No cache hit or expired, calculate result
-      } else {
-        // TODO works but weird thing to do, make a copy of args instead...
-        args.push(function() {
-          // error argument and result arguments
-          // function(err, results) ...
-          var err = arguments[0], args;
-          if (err) {
-            cb(err);
-            return;
-          }
-          args = slice.call(arguments, 1);
-          self.store.set(key, {
-            data: args,
-            expires: (Date.now() + self.maxAge)
-          });
-        })
-        fun.apply(null, args);
+      
+      // Cache hit
+      if(result) {
+        return cb(null, result);
       }
+
+      args.push(function() {
+        // error argument and result arguments
+        // function(err, results) ...
+        var err = arguments[0], result;
+        if (err) {
+          return cb(err);
+        }
+        result = slice.call(arguments, 1);
+        self.store.set(key, result);
+      });
+      fun.apply(null, args);
     });
-  }
-}
+  };
+};
---
+++ stores/RedisStore.js
@@ -0,0 +1,35 @@
+var RonnyRedis = function RonnyRedis(opts) {
+  var redis = require('redis'),
+      db = opts.db;
+    this.client = redis.createClient(db.port, db.hostname, {
+      no_ready_check: true
+    });
+  
+  this.maxAge = opts.maxAge;
+
+  if(db.auth) {
+    client.auth(redisURL.auth.split(":")[1]);
+  }
+};
+
+RonnyRedis.prototype.set = function RonnyRedisSetter(key, value, cb) {
+  this.client.psetex(key, maxAge, JSON.stringify(value), cb);
+};
+
+RonnyRedis.prototype.get = function RonnyRedisGetter(key, cb) {
+  this.client.get(key, function(err, res) {
+    if (err) {
+      cb(err);
+      return;
+    }
+    var result;
+    try {
+      result = JSON.parse(res);
+    } catch(e) {
+      return cb(e);
+    }
+    cb(null, result)
+  });
+};
+
+module.exports = RonnyRedis;
